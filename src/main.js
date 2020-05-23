419657ec74092ae290309d0d7b16debbeb8def3f
@Ishajaggi <isha@volumetree.com>
2020-05-23 12:20:40 +0530

    updated with changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue, IconsPlugin ,LayoutPlugin, CardPlugin, NavbarPlugin,NavPlugin ,DropdownPlugin,ButtonPlugin,ButtonGroupPlugin} from 'bootstrap-vue'
+
 Vue.config.productionTip = false
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(CardPlugin)
+Vue.use(LayoutPlugin)
+Vue.use(NavbarPlugin)
+Vue.use(NavPlugin)
+Vue.use(DropdownPlugin)
+Vue.use(ButtonGroupPlugin)
+Vue.use(ButtonPlugin)
+
+
+
 
 new Vue({
   render: h => h(App),
